c9c4c21f581301bd2b7035cbef25b295c9e06ab0
wuyinan.0126 <wuyinan.0126@bytedance.com>
2018-08-28 20:17:15 +0800

    update

--- coin/model_maker.py
+++ coin/model_maker.py
@@ -1,5 +1,6 @@
 import argparse
 
+import sys
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers import GRU, CuDNNGRU
@@ -35,18 +36,34 @@ class ModelMaker:
     #     model.add(Activation('relu'))
     #     model.load_weights(os.path.join(C['models_dir'], hdf5_file_name))
 
-    def train(self, h5_file_name, model_opts=C['gru_opts']):
+    def train(self, h5_file_name, model_opts):
+        def train_gru():
+            model = Sequential()
+            model.add(
+                GRU(units=model_opts['units'], input_shape=(step_size, features_size), return_sequences=False))
+            model.add(Activation('tanh'))
+            model.add(Dropout(0.2))
+            model.add(Dense(C['output_size']))
+            model.add(Activation('relu'))
+            model.compile(loss='mse', optimizer='adam')
+            model.fit(
+                training_data, training_labels,
+                batch_size=model_opts['batch_size'],
+                validation_data=(validation_data, validation_labels),
+                epochs=model_opts['epochs'],
+                callbacks=[
+                    CSVLogger(os.path.join(C['logs_dir'], model_file_name + '.csv'), append=True),
+                    ModelCheckpoint(
+                        os.path.join(C['models_dir'], model_file_name + '_{epoch:02d}_{val_loss:.5f}.hdf5'),
+                        monitor='val_loss', verbose=1, mode='min'
+                    )
+                ])
+
         with h5py.File(os.path.join(C['h5_dir'], h5_file_name), 'r') as hf:
             inputs = hf['inputs'].value
             labels = hf['outputs'].value
-        model_file_name = os.path.splitext(h5_file_name)[0] + '_' + model_opts.get('arch')
+        model_file_name = os.path.splitext(h5_file_name)[0] + '_' + model_opts['arch']
 
-        if model_opts.get('arch') == 'gru':
-            self.train_gru(model_opts, inputs, labels, model_file_name)
-        elif model_opts.get('arch') == 'lstm':
-            pass
-
-    def train_gru(self, model_opts, inputs, labels, model_file_name):
         data_size = inputs.shape[0]
         step_size = inputs.shape[1]
         features_size = inputs.shape[2]
@@ -57,25 +74,8 @@ class ModelMaker:
         validation_data = inputs[training_size:, :]
         validation_labels = labels[training_size:, :, 0]
 
-        model = Sequential()
-        model.add(GRU(units=model_opts.get('units'), input_shape=(step_size, features_size), return_sequences=False))
-        model.add(Activation('tanh'))
-        model.add(Dropout(0.2))
-        model.add(Dense(C['output_size']))
-        model.add(Activation('relu'))
-        model.compile(loss='mse', optimizer='adam')
-        model.fit(
-            training_data, training_labels,
-            batch_size=model_opts.get['batch_size'],
-            validation_data=(validation_data, validation_labels),
-            epochs=model_opts.get['epochs'],
-            callbacks=[
-                CSVLogger(os.path.join(C['logs_dir'], model_file_name + '.csv'), append=True),
-                ModelCheckpoint(
-                    os.path.join(C['models_dir'], model_file_name + '_{epoch:02d}_{val_loss:.5f}.hdf5'),
-                    monitor='val_loss', verbose=1, mode='min'
-                )
-            ])
+        # noinspection PyCallingNonCallable
+        locals().get('train_' + model_opts['arch'])()
 
 
 if __name__ == '__main__':
